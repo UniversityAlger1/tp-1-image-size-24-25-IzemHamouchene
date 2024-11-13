276ce4723483fcf73be48ce0ece5d408308c0937
Izem <izem.hamouchene@soljit.com>
2024-11-13 16:39:27 +0100

    fixe typo

--- config/mainVideo.c
+++ config/mainVideo.c
@@ -6,18 +6,18 @@
 #include "video.h"
 #include "assert_with_message.h"
 
-int testMethod(int input1, int input2, int durationMovie, int durationCredit, int fps, char* unit, int output){
+int testMethod(int input1, int input2, int durationMovie, int durationCredit, int fps, char* unit, float output){
     char message[100];
-    int result = video(input1, input2, durationMovie, durationCredit, fps, unit);
-    sprintf(message, "Inputs (%d, %d). Expected output should be %d. Actuel output = %d", input1, input2, output, result);
+    float result = video(input1, input2, durationMovie, durationCredit, fps, unit);
+    sprintf(message, "Inputs (%d, %d). Expected output should be %f. Actuel output = %f", input1, input2, output, result);
     assert_with_message(((result >= output - 0.2) && (result <= output + 0.2)), message);
 }
 
 int main() {
     testMethod(1024, 1024, 30, 10, 20, "go", 1.953125);
 
-    testMethod(500, 300, 60, 10, 20, "mo", 429.1534424);
+    testMethod(500, 300, 60, 10, 20, "mo", 543.5943604);
 
-    testMethod(500, 400, 10, 2, 1, "ko", 13671.875);
+    testMethod(500, 400, 10, 2, 1, "ko", 6250);
     return 0;
 }
