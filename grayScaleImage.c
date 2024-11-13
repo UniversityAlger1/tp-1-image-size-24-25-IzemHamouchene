36eea62cb5d75b62b8f38b2f2faf343c71194143
Izem <izem.hamouchene@soljit.com>
2024-11-13 14:38:48 +0100

    fix typo

--- grayScaleImage.c
+++ grayScaleImage.c
@@ -3,7 +3,7 @@
 //   w: width of the image
 //   h: height of the image
 // Return value
-//   grayScale size of the image in 'bytes'
+//   grayScale size of the image Bitmap (in byte)
 int grayScaleImage(int w, int h) {
    // YOUR CODE HERE - BEGIN
 
--- video.c
+++ video.c
@@ -2,9 +2,9 @@
 // Parameters:
 //   w: width of the image
 //   h: height of the image
-//   durationMovie: duration in second of movie
-//   durationCredits: duration in second of credit
-//   unit: Unit of the output value. It could be 'bt' bits, 'ko' kilobits, 'mo' megabits, 'go' gigabits
+//   durationMovie: duration in second of movie (colored image)
+//   durationCredits: duration in second of credit (image Black/White)
+//   unit: Unit of the output value. It could be 'bt' byte, 'ko' kilobits, 'mo' megabits, 'go' gigabits
 // Return value
 //   colored video size (based on the unit passed parametter)
 float video(int w, int h, int durationMovie, int durationCredits, int fps, char* unit) {
